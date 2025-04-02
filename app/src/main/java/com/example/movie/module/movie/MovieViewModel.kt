3ba90120762c1e7854a3ef147f4cb5a9556bc7a4
Phanthai1601 <phanhoangthai765@gmail.com>
2025-04-02 10:48:39 +0700

    add viewmodel movie, fix fetch API error

--- app/src/main/java/com/example/movie/module/movie/MovieViewModel.kt
+++ app/src/main/java/com/example/movie/module/movie/MovieViewModel.kt
@@ -1,46 +1,66 @@
 package com.example.movie.module.movie
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.movie.model.Items
 import com.example.movie.model.Movie
 import com.example.movie.model.response.ListMovieResponse
+import com.example.movie.model.response.MovieDetailResponse
 import com.example.movie.repository.MovieRepository
 
 class MovieViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _movies = MutableLiveData<ArrayList<Items>>()
     val movies: LiveData<ArrayList<Items>> get() = _movies
-    private val _movieDetail = MutableLiveData<Movie>()
-    val movieDetail: LiveData<Movie> get() = _movieDetail
-
+    private val _movieDetail = MutableLiveData<MovieDetailResponse>()
+    val movieDetail: LiveData<MovieDetailResponse> get() = _movieDetail
 
     private val _error = MutableLiveData<String>()
     val error: LiveData<String> get() = _error
-    private  var page = (1..100).random()
 
+    private  var page = (1..100).random()
     fun fetchMovies() {
-        MovieRepository.getListMovie(
-            getApplication(),
-            page,
-            onSuccess = { movieListResponse: ListMovieResponse ->
-                _movies.postValue(movieListResponse.items)
-            },
-            onError = {}
-        )
+        val allMovies = ArrayList<Items>()
+
+        val pagesToFetch = listOf(page, page + 1,page + 2)
+        var completedRequests = 0
+
+        for (p in pagesToFetch) {
+            MovieRepository.getListMovie(
+                getApplication(),
+                p,
+                onSuccess = { movieListResponse: ArrayList<Items> ->
+                    allMovies.addAll(movieListResponse)
+                    completedRequests++
+
+                    if (completedRequests == pagesToFetch.size) {
+                        _movies.postValue(allMovies)
+                    }
+                },
+                onError = { errorMessage ->
+                    Log.e("MovieRepository", "Error fetching movies: $errorMessage")
+                }
+            )
+        }
     }
-    private  var slug = "ngoi-truong-xac-song"
-    fun getMovieDetail(){
-        MovieRepository.getListMovieDetail(
+
+    fun fetchMovieDetail(slug: String){
+        MovieRepository.getMovieDetail(
             getApplication(),
             slug,
-            onSuccess = {
-                movieDetail: Movie ->
+            onSuccess ={
+                movieDetail: MovieDetailResponse->
                 _movieDetail.postValue(movieDetail)
+            },
+            onError = {
+                error-> Log.e("MovieRepository"," Error fetching movie detail")
             }
-        ) {}
 
+        )
     }
+
+
 }
--- app/src/main/java/com/example/movie/repository/MovieRepository.kt
+++ app/src/main/java/com/example/movie/repository/MovieRepository.kt
@@ -2,14 +2,16 @@ package com.example.movie.repository
 
 import android.content.Context
 import android.util.Log
+import com.bumptech.glide.load.HttpException
 import com.example.movie.model.Items
 import com.example.movie.model.Movie
-import com.example.movie.model.response.ListMovieDetailResponse
 import com.example.movie.model.response.ListMovieResponse
+import com.example.movie.model.response.MovieDetailResponse
 import com.example.movie.network.ApiRequest
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.IOException
 
 object MovieRepository {
     fun getListMovie(
@@ -40,7 +42,48 @@ object MovieRepository {
 
 
     }
+    fun getMovieDetail(
+        context: Context,
+        slug: String,
+        onSuccess: (MovieDetailResponse)->Unit,
+        onError: (message: String) -> Unit
+    ){
+        ApiRequest.getInstance().init(context)
+        ApiRequest.getInstance().getMovieDetail(slug,
+            object :Callback<MovieDetailResponse>{
+                override fun onResponse(
+                    p0: Call<MovieDetailResponse>,
+                    p1: Response<MovieDetailResponse>
+                ) {
+                    val response = p1.body()
+                    if(response!=null){
+                        onSuccess.invoke(response)
+                    }
+                }
 
+                override fun onFailure(p0: Call<MovieDetailResponse>, p1: Throwable) {
+                    Log.e("API_ERROR", "Request failed", p1)
+
+                    when (p1) {
+                        is IOException -> {
+                            // Lỗi kết nối mạng
+                            Log.e("API_ERROR_DEMO", "Network failure, please check your internet connection.")
+                        }
+                        is HttpException -> {
+                            // Lỗi HTTP từ server
+                            Log.e("API_ERROR_DEMO1", "HTTP error: ${p1.message}")
+                        }
+                        else -> {
+                            // Lỗi không xác định
+                            Log.e("API_ERROR_DEMO2", "Unexpected error: ${p1.localizedMessage}")
+                        }
+                    }
+                }
+
+
+            }
+        )
+    }
 
 
 }
